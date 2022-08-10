d2bf99916c6a795f3a9cdac26b4689467173ad10
phildong <zhe.dong@icahn.mssm.edu>
2022-08-10 02:02:05 +0000

    update helper script to reorganize data

--- helper_scripts/reorganize_data.py
+++ helper_scripts/reorganize_data.py
@@ -1,6 +1,7 @@
 #%% imports and definitions
 import os
 import shutil
+import warnings
 
 import pandas as pd
 import xarray as xr
@@ -11,16 +12,18 @@ IN_RAW_PATH = "/home/jovyan/ExpanDrive/Dropbox/06. Metacell analysis/Data/Raw vi
 IN_DS_FNAME = "minian_dataset.nc"
 IN_CP_FILES = ["arduinolog.txt", "timeStamps.csv"]
 IN_MP_FILE = "mappings.pkl"
+IN_SS_CSV = "../metadata/sessions.csv"
 DRY_RUN = False
 OUT_SS_PATH = "../metadata/sessions.csv"
 OUT_PS_PATH = "../intermediate/processed"
 OUT_MP_PATH = "../intermediate/cross_reg"
-OUT_RAW_PATH = "../data"
+OUT_RAW_PATH = "/home/jovyan/ExpanDrive/Dropbox/06. Metacell analysis/Data/raw"
 
 os.makedirs(OUT_PS_PATH, exist_ok=True)
 os.makedirs(OUT_MP_PATH, exist_ok=True)
-ss_ls = []
 
+#%% load session csv
+sessions = pd.read_csv(IN_SS_CSV).set_index("data")
 
 #%% walk through folders and copy data
 for root, dirs, files in os.walk(IN_PS_PATH):
@@ -28,24 +31,15 @@ for root, dirs, files in os.walk(IN_PS_PATH):
         ds = xr.open_dataset(os.path.join(root, IN_DS_FNAME))
         path_ls = root.split(os.sep)
         ss_type, anm, day = path_ls[-3], path_ls[-1], path_ls[-2]
-        ss = ds.coords["session"].values.item()
+        date = ds.coords["session"].values.item()
+        dpath = os.path.join(anm, date, "My_V4_Miniscope")
+        meta = sessions.loc[dpath]
+        ss = meta["session"]
         ds = ds.assign_coords(animal=anm, session=ss)
         ps_path = os.path.join(OUT_PS_PATH, "{}-{}.nc".format(anm, ss))
-        raw_path = os.path.join(OUT_RAW_PATH, anm, ss, "My_V4_Miniscope")
+        raw_path = os.path.join(OUT_RAW_PATH, dpath)
         for fname in IN_CP_FILES:
             assert fname in files
-        row = pd.DataFrame(
-            [
-                {
-                    "animal": anm,
-                    "session": ss,
-                    "session_type": ss_type,
-                    "day": day,
-                    "data": os.path.relpath(raw_path, OUT_RAW_PATH),
-                }
-            ]
-        )
-        ss_ls.append(row)
         print("saving data to {}".format(ps_path))
         print("copying files to {}".format(raw_path))
         if not DRY_RUN:
@@ -57,39 +51,47 @@ for root, dirs, files in os.walk(IN_PS_PATH):
                 )
 
 #%% walk through folders and copy mappings
+sessions_anm = sessions.reset_index().set_index("animal")
 for root, dirs, files in os.walk(IN_MAP_PATH):
     if IN_MP_FILE in files:
         mp = pd.read_pickle(os.path.join(root, IN_MP_FILE))
         anm = root.split(os.sep)[-1]
+        ss_map = (
+            sessions_anm.loc[anm][["date", "session"]]
+            .set_index("date")["session"]
+            .to_dict()
+        )
         mp["variable", "animal"] = anm
+        mp = mp.rename(columns=ss_map).drop(columns=[("group", "group")])
+        assert all(c.startswith("Day") for c in mp["session"].columns)
         save_path = os.path.join(OUT_MP_PATH, "{}.pkl".format(anm))
         print("saving to {}".format(save_path))
         if not DRY_RUN:
             mp.to_pickle(save_path)
 
-#%% walk through folders and make symlink
+#%% walk through folders and move files
 for root, dirs, files in os.walk(IN_RAW_PATH):
     path_ls = os.path.relpath(root, IN_RAW_PATH).split(os.sep)
-    if len(path_ls) == 4:
-        cohort, anm, ss, time = path_ls
-        outpath = os.path.join(OUT_RAW_PATH, anm, ss, time)
-        row = pd.DataFrame(
-            [
-                {
-                    "animal": anm,
-                    "session": ss,
-                    "cohort": cohort,
-                    "data": os.path.relpath(outpath, OUT_RAW_PATH),
-                }
-            ]
-        )
-        ss_ls.append(row)
-        print("linking data from {} to {}".format(root, outpath))
+    if path_ls[-1] == "My_V4_Miniscope":
+        if len(path_ls) == 4:
+            anm, date, time, _ = path_ls
+        elif len(path_ls) == 3:
+            anm, date, _ = path_ls
+        else:
+            continue
+        outpath = os.path.join(anm, date, "My_V4_Miniscope")
+        meta_row = sessions.loc[outpath]
+        assert isinstance(meta_row, pd.Series)
+        outpath = os.path.join(OUT_RAW_PATH, outpath)
+        print("copying data from {} to {}".format(root, outpath))
         if not DRY_RUN:
-            os.makedirs(os.path.dirname(outpath), exist_ok=True)
-            os.symlink(root, outpath)
-
-#%% write session csv
-sessions = pd.concat(ss_ls, ignore_index=True).sort_values(["animal", "session"])
-os.makedirs(os.path.dirname(OUT_SS_PATH), exist_ok=True)
-sessions.to_csv(OUT_SS_PATH)
+            if os.path.exists(outpath):
+                shutil.rmtree(outpath, ignore_errors=True)
+            os.makedirs(outpath, exist_ok=True)
+            for fname in IN_CP_FILES:
+                try:
+                    shutil.copyfile(
+                        os.path.join(root, fname), os.path.join(outpath, fname)
+                    )
+                except FileNotFoundError:
+                    warnings.warn("cannot find {}".format(os.path.join(root, fname)))
